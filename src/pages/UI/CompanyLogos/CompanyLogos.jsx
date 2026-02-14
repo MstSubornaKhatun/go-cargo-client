68ec7d4c1f0dd87758f3880a22a8c5d9056c14f1
suborna33 <152749142+suborna33@users.noreply.github.com>
2026-02-14 13:33:39 +0600

    marquee

---
+++ src/pages/UI/CompanyLogos/CompanyLogos.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import Marquee from 'react-fast-marquee';
+import companyLogo1 from '../../../assets/brands/amazon.png'
+import companyLogo2 from '../../../assets/brands/amazon_vector.png'
+import companyLogo3 from '../../../assets/brands/casio.png'
+import companyLogo4 from '../../../assets/brands/moonstar.png'
+import companyLogo5 from '../../../assets/brands/randstad.png'
+import companyLogo6 from '../../../assets/brands/start-people 1.png'
+import companyLogo7 from '../../../assets/brands/start.png'
+
+const CompanyLogos = () => {
+    return (
+        <div>
+                <div className="py-16 bg-base-100">
+      <div className="text-center mb-10">
+        <h2 className="text-3xl font-bold">
+          We've helped thousands of sales teams
+        </h2>
+      </div>
+
+      <Marquee 
+        speed={50} 
+        gradient={false} 
+        pauseOnHover={true}
+        direction="left"
+      >
+        <div className="flex gap-16 items-center">
+          <img src={companyLogo1} className="" alt="logo" />
+          <img src={companyLogo2} className="" alt="logo" />
+          <img src={companyLogo3} className="" alt="logo" />
+          <img src={companyLogo4} className="" alt="logo" />
+          <img src={companyLogo5} className="" alt="logo" />
+          <img src={companyLogo6} className="" alt="logo" />
+          <img src={companyLogo7} className="" alt="logo" />
+        </div>
+      </Marquee>
+</div>
+        </div>
+    );
+};
+
+export default CompanyLogos;
